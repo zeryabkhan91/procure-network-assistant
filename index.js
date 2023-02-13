fd62678ebec217f13225085a930a9f9ec005aac1
Zeryab Khan <zeryabkhan91@gmail.com>
2023-02-13 22:59:42 +0500

    integrated with procure-backend

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 
 const path = require("path");
 const systemInfo = require("./src/helper");
-const mongoConnect = require("./src/database/database");
 const AutoLaunch = require("auto-launch");
-const createAsset = require("./src/controllers/AssetController");
+const { default: fetch } = require("electron-fetch");
 
 let win;
 app.whenReady().then(() => {
@@ -39,16 +38,115 @@ app.whenReady().then(() => {
   });
 });
 
+const callQuery = async () => {
+  const systemInformation = await systemInfo();
+  const { memMax, memSlots, ...motherboardRequired } =
+    systemInformation.baseboard;
+  const { cache, ...cpuRequired } = systemInformation.cpu;
+  const networkRequired = systemInformation.net.map((network) => {
+    const {
+      speed,
+      dnsSuffix,
+      ieee8021xAuth,
+      ieee8021xState,
+      carrierChanges,
+      ...networkRequired
+    } = network;
+    return networkRequired;
+  });
+
+  const displayRequired = systemInformation.graphics.displays.map((display) => {
+    const {
+      sizeX,
+      sizeY,
+      pixelDepth,
+      resolutionX,
+      resolutionY,
+      positionX,
+      positionY,
+      ...displayRequired
+    } = display;
+    return displayRequired;
+  });
+
+  const storageRequired = systemInformation.diskLayout.map((storage) => {
+    const {
+      bytesPerSector,
+      totalCylinders,
+      totalHeads,
+      totalSectors,
+      totalTracks,
+      tracksPerCylinder,
+      sectorsPerTrack,
+      temperature,
+      ...storageRequired
+    } = storage;
+    return storageRequired;
+  });
+
+  const diskRest = systemInformation.fsSize.map((disk) => {
+    const { fs, rw, ...diskRest } = disk;
+    return { fileSystem: fs, readWrite: rw, ...diskRest };
+  });
+
+  const { list, ...processRequired } = systemInformation.processes;
+
+  fetch("http://localhost:6100/api/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbElkIjoicmVwb3Rlc3Q5MTdAZ21haWwuY29tIiwidGVuYW50SWQiOiI2MjgwYzI2ZGRkNzdhNjQzNjY2ZDY5MmIiLCJ0b2tlblVzZSI6ImFjY2VzcyIsInVzZXJJZCI6IjYzNDAxZTIzODZhZjBiNjEyMTA1OGRhZiIsImlhdCI6MTY3NjMwOTc1NCwiZXhwIjoxNjc2Mzk2MTU0fQ._res7x7PkAZcle-MU--z8RL_blFC919JVirUH9iyZu4",
+    },
+    body: JSON.stringify({
+      query: `
+      mutation ($input: CreateNetworkAssetsInput!) {
+        createNetworkAsset(
+          input: $input
+        ) {
+          success
+          networkAssets {
+            id
+          }
+        }
+      }
+      `,
+      variables: {
+        input: {
+          userName: systemInformation.os.hostname,
+          battery: systemInformation.battery,
+          system: systemInformation.system,
+          bios: {
+            vendor: systemInformation.bios.vendor,
+            version: systemInformation.bios.version,
+            releaseDate: systemInformation.bios.releaseDate,
+          },
+          motherboard: motherboardRequired,
+          operatingSystem: systemInformation.os,
+          cpu: cpuRequired,
+          graphics: systemInformation.graphics.controllers,
+          display: displayRequired,
+          network: networkRequired,
+          memory: systemInformation.mem,
+          storage: storageRequired,
+          disk: diskRest,
+          port: systemInformation.networkConnections,
+          process: processRequired,
+        },
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      if (result.data.createNetworkAsset.success) {
+        win.webContents.send("asynchronous-message", { loading: false });
+      }
+    });
+};
+
 const asyncfunc = async () => {
   win.webContents.send("asynchronous-message", { loading: true });
-  mongoConnect();
-  const systemInformation = await systemInfo();
-  const response = await createAsset(systemInformation);
-  console.log(response);
-  if (response) {
-    console.log(response);
-    win.webContents.send("asynchronous-message", { loading: false });
-  }
+  callQuery();
 };
 
 app.on("open-url", function (event, data) {
--- src/helper.js
+++ src/helper.js
@@ -2,14 +2,7 @@ const si = require("systeminformation");
 
 const systemInfo = async () => {
   const systemInformation = await si.getAllData();
-  // console.log(systemInformation, "system information");
   return systemInformation;
 };
-
-// const createAsset = async (systemInfo) => {
-//   const macAddress = systemInfo.uuid.macs[0];
-//   const id = await createAsset();
-//   return id;
-// };
 
 module.exports = systemInfo;
